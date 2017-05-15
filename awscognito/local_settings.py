2718ea0615aa9a8603999700fb6cc8c89a089f3c
Brian Guggenheimer <bguggs@gmail.com>
2017-05-15 12:42:09 -0700

    added local settings file

---
+++ awscognito/local_settings.py
@@ -0,0 +1,19 @@
+#AWS COGNITO CREDENTIALS
+
+IDENTITYPOOLID = 'us-west-2:d2c09ce1-8327-45da-9c60-ae5de918ab48'
+
+ACCOUNTID = '491781401898'
+
+AWS_ACCESS_KEY = 'AKIAJKZ5YGJUTFOHJXCQ'
+
+AWS_SECRET_KEY = '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'
+
+DEFAULT_REGION_NAME = 'us-west-2'
+
+DEFAULT_USER_POOL_ID = 'us-west-2_8O21f5pZg'
+
+DEFAULT_USER_POOL_APP_ID = '2kotn8s2a0ssfab7qfce8rd8lb' #APP should not have any secret in AWS
+
+DEFAULT_CONFIG = {'region_name':DEFAULT_REGION_NAME, 'aws_access_key_id':AWS_ACCESS_KEY, 'aws_secret_access_key':AWS_SECRET_KEY}
+
+DEFAULT_USER_POOL_LOGIN_PROVIDER = 'cognito-idp.us-west-2.amazonaws.com/us-west-2_8O21f5pZg'
